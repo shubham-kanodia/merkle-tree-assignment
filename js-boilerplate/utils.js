3d1495c3cfdd467214937e86080e66bc25070950
Harsh Agrawal <itsharshag@gmail.com>
2022-10-06 17:24:22 +0530

    Update README and add additional util functions

--- js-boilerplate/utils.js
+++ js-boilerplate/utils.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import csvtojson from "csvtojson";
 import { Parser } from "json2csv";
+import crypto from "crypto";
 
 export const readCSV = async (filePath) => await csvtojson().fromFile(filePath);
 
@@ -8,3 +9,8 @@ export const writeJSON = (json, filePath) => {
   const csv = new Parser().parse(json);
   fs.writeFileSync(filePath, csv);
 };
+
+export const sha256 = (tx) => crypto.createHash("sha256").update(tx).digest();
+
+export const hashPair = (hashA, hashB, hashFunction = sha256) =>
+  hashFunction(Buffer.concat([hashA, hashB]));
